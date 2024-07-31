00d61c64a8a2791a0818e303c9a3dc8c2bfe9947
joaopandolfi <joaopandolfi@gmail.com>
2024-07-31 14:27:40 -0300

    reordering waf

--- server/web/controllers/proxy/routers.go
+++ server/web/controllers/proxy/routers.go
@@ -11,12 +11,12 @@ func (c *controller) SetupRouter(s *server.Server) {
 	c.s = s
 	handler := c.proxy
 
-	if config.Get().Pow.Active {
-		handler = middleware.PoW(handler)
-	}
-
 	if config.Get().Waf.Active {
 		handler = middleware.Waf(handler)
+	}
+
+	if config.Get().Pow.Active {
+		handler = middleware.PoW(handler)
 	}
 
 	c.s.R.PathPrefix("/").HandlerFunc(handler)
